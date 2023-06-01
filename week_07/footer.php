332b71bbb4c30d3771d3dd369d22606c5a40f970
heshan8 <heshan988@gmail.com>
2023-06-01 22:23:02 +1000

    Create header and footer

---
+++ week_07/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p> &copy; <?php echo date("Y"); ?> HighJinks All rights reserved.</p>
+</footer>
---
+++ week_07/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="english">
+<head>
+
+    <title>HighJinks</title>
+</head>
+<body>
+<header>
+    <h1>Welcome to HighJinks</h1>
+
+</header>
