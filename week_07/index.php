c7a996e86b00482ce541a879b5ab26a466a51ef6
heshan8 <heshan988@gmail.com>
2023-06-01 22:29:13 +1000

    Create Index page

---
+++ week_07/index.php
@@ -0,0 +1,15 @@
+<?php include ('header.php'); include ("nav.php"); ?>
+<link rel="stylesheet" href="style.css">
+<section>
+    <div id="welcome_image">
+        <img src="/week_01/images/office_prank.jpg" alt="Prank Image">
+        <div class="centered">Welcome to high jinks</div>
+    </div>
+    <p>We are a professional group of pranksters looking to make people happy
+        Welcome to High jinks, where we believe laughter is the best medicine. Our team of professional pranksters is
+        dedicated to bringing joy and excitement to your life through our creative and innovative pranks, anywhere 
+        and any time.
+
+        Our mission is to provide high-quality, safe, and hilarious pranks that are guaranteed to put a smile on your face. We understand the importance of laughter and how it can positively impact your day, which is why we strive to deliver the best pranking experience possible.</p>
+</section>
+<?php include 'footer.php'; ?>
---
+++ weekly_learning_journals/week_07 journal.md
@@ -0,0 +1,21 @@
+# CP3402 Weekly Journal
+
+## Week 7
+
+### Learning activities
+Watching lecture content.  
+Watched LinkedIn Learning PHP course Videos.
+Reading PHP documentation
+
+
+### Resources and links
+- https://www.linkedin.com/learning/learning-php-2
+- https://www.php.net/manual/en/
+
+### Estimated hours
+Around 3 hours.
+
+### Content insights
+
+
+### Career/Employability/Learning Insights
