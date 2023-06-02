8058c6779525b6f50acf8d705d01f0f289420c84
heshan8 <heshan988@gmail.com>
2023-06-03 01:30:45 +1000

    Add files from week 7 practical

---
+++ week_09/about_us.php
@@ -0,0 +1,43 @@
+<?php include ('header.php'); include ("nav.php"); ?>
+<link rel="stylesheet" href="mainstyles.css">
+<section>
+    <h1 id="about_us_1">Some of our talented team members</h1>
+    <div class="row">
+        <div class="card">
+            <img src="/week_01/images/will.jpg" alt="Jane" style="width:30%">
+            <div class="container">
+                <h2>Will campbell</h2>
+                <p class="title">CEO &amp; Founder</p>
+                <p></p>
+                <p>will.campbel@jinks.com</p>
+            </div>
+        </div>
+
+
+
+        <div class="card">
+            <img src="/week_01/images/ronnie.jpg" alt="Mike" style="width:30%">
+            <div class="container">
+                <h2>Ronnie Smith</h2>
+                <p class="title">Prop Designer &amp; Founder</p>
+                <p></p>
+                <p>ronnie.smith@jinks.com</p>
+
+            </div>
+        </div>
+
+
+
+        <div class="card">
+            <img src="/week_01/images/emily.jpg" alt="John" style="width:30%">
+            <div class="container">
+                <h2>Emily Wood</h2>
+                <p class="title">Locations Manager</p>
+                <p></p>
+                <p>emily.wood@jinks.com</p>
+            </div>
+        </div>
+    </div>
+
+</section>
+<?php include 'footer.php'; ?>
---
+++ week_09/index.php
@@ -0,0 +1,19 @@
+<?php include ('header.php'); include ("nav.php"); ?>
+<link rel="stylesheet" href="mainstyles.css">
+<section>
+    <div id="welcome_image">
+        <img src="/week_01/images/office_prank.jpg" alt="Prank Image">
+        <div class="centered">Welcome to high jinks</div>
+    </div>
+    <p>We are a professional group of pranksters looking to make people happy
+        Welcome to High jinks, where we believe laughter is the best medicine. Our team of professional pranksters is
+        dedicated to bringing joy and excitement to your life through our creative and innovative pranks, anywhere 
+        and any time.
+
+        Our mission is to provide high-quality, safe, and hilarious pranks that are guaranteed to put a smile on your face. We understand the importance of laughter and how it can positively impact your day, which is why we strive to deliver the best pranking experience possible.
+    </p>
+    <a href="#" class="button">Get Started</a>
+    <a href="#" class="button_2">Contact Us</a>
+<?php include 'footer.php'; ?>  
+</section>
+
